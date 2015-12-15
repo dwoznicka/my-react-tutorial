c5dca5c1ec8b1c822001c509f1efff08ae514c2a
Daria Woznicka <woznicka.daria@gmail.com>
2015-12-15 13:02:33 +0100

    step 5 - states

--- views/index.jsx
+++ views/index.jsx
@@ -28,14 +28,26 @@ class TodoList extends React.Component {
 }
 
 class Todo extends React.Component {
-  render() {
-    return (
-      <tr>
-        <td style={{border: "1px solid black"}}>{this.props.title}</td>
-        <td style={{border: "1px solid black"}}>{this.props.children}</td>
-      </tr>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {checked: false};
+    }
+
+    handleChange(e) {
+        this.setState({checked: e.target.checked});
+    }
+
+    render() {
+        return (
+            <tr>
+                <td style={{border: "1px solid black"}}>
+                    <input type="checkbox" checked={this.state.checked} onChange={this.handleChange}/>
+                </td>
+                <td style={{border: "1px solid black"}}>{this.props.title}</td>
+                <td style={{border: "1px solid black"}}>{this.props.children}</td>
+            </tr>
+        );
+    }
 }
 Todo.propTypes = {
     title: React.PropTypes.string.isRequired
